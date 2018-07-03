3b8d0ab39c0ca3402794aee1efc33f6e3e06237e
Ingo Mohr <tellastory73@gmail.com>
2018-07-03 23:25:40 +0200

    added first dynamic stuff: data from object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,41 @@
 
 console.log('App.js is up and running.');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: "Don't just sit there, do something!"
+};
+
 // JSX - Javascript XML
 var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 console.log('App.js is up and running.');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: "Don't just sit there, do something!"
+};
+
 // JSX - Javascript XML
-var template = <p>This is JSX from app!</p>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
