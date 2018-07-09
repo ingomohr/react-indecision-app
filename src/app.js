48dd4b264c5b9798a87dac14f75342c0234ab7fc
Ingo Mohr <tellastory73@gmail.com>
2018-07-09 22:17:42 +0200

    Moved counter example to own script file

--- src/app.js
+++ src/app.js
@@ -46,39 +46,8 @@ var template = (
     </div>
 );
 
-let count = 0;
-const someId = "someId";
-
-const addOne = () => {
-    count++;
-    renderCounterApp();
-}
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
-
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne} className="button">+1</button>
-            <button onClick={minusOne} className="button">-1</button>
-            <button onClick={reset} className="button">Reset</button>
-        </div>
-    );
-    ReactDOM.render(templateTwo, appRoot);
 
-}
-
-renderCounterApp();
 
 
