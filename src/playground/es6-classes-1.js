18e452f80545b974d3d9da72cc0e35a500a182b0
Ingo Mohr <tellastory73@gmail.com>
2018-07-11 22:14:08 +0200

    adding a subclass

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -21,3 +21,38 @@ const person2 = new Person("Leeroy Jenkins", 29);
 console.log(person2);
 console.log(person2.sayHi());
 
+/**
+ * A student.
+ * <p>
+ * And <i>this</i> is some lovely class comment.
+ * </p>
+ * ... which - sadly - doesn't support html tags
+ * ... at least not in my VSCode installation.
+ */
+class Student extends Person {
+
+    constructor(name, age, university) {
+        super(name, age);
+        this.university = university;
+    }
+
+    isInUniversity() {
+        // '!!' => flipped twice
+        // => is true if this.univerity is set
+        return !!this.university;
+    }
+
+    sayHi() {
+        let hi = super.sayHi();
+        return hi + ". Oh, and I'm a student at " + this.university;
+    }
+
+
+}
+const student = new Student();
+console.log(student);
+console.log(student.isInUniversity());
+student.university = "Oxford";
+console.log(student.isInUniversity());
+console.log(student.sayHi());
+
