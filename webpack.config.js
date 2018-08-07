adea1339abc4608ed15d3facd6f048a51e82b5ac
Ingo Mohr <tellastory73@gmail.com>
2018-08-07 22:47:47 +0200

    added source-map to make stacktraces jump to src instead of bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-// entry -> output
+// Everytime you change this file, you need to
+// restart the webpack build (yarn run build)
 
 // see webpack.js.org for docs & stuff
 
@@ -14,13 +15,15 @@ module.exports = {
   module: {
     rules: [
       {
+        // loader: we use this to enable transform JSX using Babel
+        // first -> yarn install babel-core babel-loader
         loader: "babel-loader",
         test: /\.js$/,
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  // Maps stacktraces in the dev console (browser) to the source code
+  // instead of the line in the bundle.js
+  devtool: "cheap-module-eval-source-map"
 };
-
-// loader: we use this to enable transform JSX using Babel
-// first -> yarn install babel-core babel-loader
